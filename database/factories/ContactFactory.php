e429032199e18f200aa4d717eb2a8d276ceaacff
Agacob <eitoruczar@gmail.com>
2020-09-07 16:06:02 -0300

    Seed

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Contact::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'email'=> $faker->unique()->safeEmail,
+        'phone'=> $faker->phoneNumber,
     ];
 });
